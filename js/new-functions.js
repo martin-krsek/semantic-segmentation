f981faf59a4ab66571e0fcc2cbfaa4df269a723f
Fullajtar <fullajtar.dominik@gmail.com>
2021-11-13 12:42:37 +0100

    Init - some admin functions FE Bugs - edit/preview arent centered (scrollbar issue), newfunction.js #20

--- js/new-functions.js
+++ js/new-functions.js
@@ -17,7 +17,30 @@ function hideLogin() {
 }
 
 // https://stackoverflow.com/a/28392348
-function changeCss () {
+function changeCss () { //TODO set scroll to dynapic value -> prevent bugs after editing page
     console.log(scrollY)
     this.scrollY < 2450 ? hideLogin() : console.log('nope');
 }
+
+$('#edit').click(function() {
+    window.scrollBy(document.querySelector('#preview-part').offsetWidth * -1 - 20,0)}); //20 scrollbar
+
+$('#preview').click(function() {
+    window.scrollBy(document.querySelector('#editing-part').offsetWidth + 20,0) //20 scrollbar
+});
+
+console.log()
+console.log(document.getElementById("home").style.height)
+
+
+setTimeout(function(){
+    document.getElementById("home-edit").style.height = document.querySelector('#home').clientHeight + "px"
+}, 500);
+
+document.getElementById("about-edit").style.height = document.querySelector('#about').clientHeight + "px"
+document.getElementById("projects-edit").style.height = document.querySelector('#projects').clientHeight + "px"
+
+
+// home
+// about
+// projects
